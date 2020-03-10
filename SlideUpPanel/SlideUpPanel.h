cd0d5ae98ac8f39cbe47690597e3deb24d3aa0b4
Dennis <dennis.rodrigues@aisdigital.com.br>
2020-03-10 09:25:44 -0300

    Initial Commit

---
+++ SlideUpPanel/SlideUpPanel.h
@@ -0,0 +1,19 @@
+//
+//  SlideUpPanel.h
+//  SlideUpPanel
+//
+//  Created by Dennis Merli on 10/03/20.
+//  Copyright © 2020 Dennis Merli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SlideUpPanel.
+FOUNDATION_EXPORT double SlideUpPanelVersionNumber;
+
+//! Project version string for SlideUpPanel.
+FOUNDATION_EXPORT const unsigned char SlideUpPanelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideUpPanel/PublicHeader.h>
+
+
